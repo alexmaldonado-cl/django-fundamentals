abc7ce15fbc4199b4652ca6ce536bbec9c1d8f5a
Alex Maldonado <alex.maldonado@outlook.com>
2023-02-17 09:08:39 -0300

    Added initial views and templates

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estás en la página principal")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta ID {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta ID {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando por la pregunta ID {question_id}")
