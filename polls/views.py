9a74ba88d0bdef4f0ef590906e54073dbe503b22
Alex Maldonado <alex.maldonado@outlook.com>
2023-02-22 16:19:47 -0300

    Added form to vote and view for show results

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.utils.html import escape
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
 
 # Create your views here.
@@ -18,8 +20,27 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estás viendo los resultados de la pregunta ID {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, "polls/results.html", {
+        'question': question,
+    })
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando por la pregunta ID {question_id}")
+
+    question = get_object_or_404(Question, pk=question_id)
+    choice   = request.POST.get('choice')
+
+    try:
+        selected_choice = question.choice_set.get(pk=choice)
+    except:
+        return render(request, "polls/detail.html", {
+            'question': question,
+            'error_message': "no elegiste una respuesta",
+        })
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=({question.id})))
