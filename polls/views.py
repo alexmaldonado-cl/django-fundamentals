a4b8f50614d65c4b09200d948d373edb808e654d
Alex Maldonado <alex.maldonado@outlook.com>
2023-03-02 08:47:28 -0300

    - Updated urls and views - Added first test for Question model

--- polls/views.py
+++ polls/views.py
@@ -4,27 +4,45 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question
 
+from django.views import generic
+
+class IndexView(generic.ListView):
+    print("")
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all()
-    print(latest_question_list)
-    return render(request, "polls/index.html", {
-        "latest_question_list": latest_question_list,
-    });
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {
+#         "latest_question_list": latest_question_list,
+#     });
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    return render(request, "polls/detail.html", {
-        "question": question,
-    });
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, id=question_id)
+#     return render(request, "polls/detail.html", {
+#         "question": question,
+#     });
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
 
-    return render(request, "polls/results.html", {
-        'question': question,
-    })
+#     return render(request, "polls/results.html", {
+#         'question': question,
+#     })
 
 
 def vote(request, question_id):
