e7be59347c342081a3d9a35e0953779b89fe265d
Alex Maldonado <alex.maldonado@outlook.com>
2023-03-02 09:00:36 -0300

    Added new test for Question model

--- polls/tests.py
+++ polls/tests.py
@@ -10,10 +10,43 @@ from .models import Question
 
 class QuestionModelTests(TestCase):
 
+    def setUp(self) -> None:
+        self.question =  Question(question_text="¿Quién es el mejor arquero del mundo?")
+
     def test_was_published_recently_with_future_questions(self):
         """was_published_recently returns False for questions whose pub_date is in the future"""
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="¿Quién es el mejor arquero del mundo?", pub_date=time)
+        time                     = timezone.now() + datetime.timedelta(days = 30)
+        
+        future_question          = self.question
+        future_question.pub_date = time
 
         # self.assertIs(future_question.was_published_recently(), False)
         self.assertFalse(future_question.was_published_recently())
+
+    def test_was_published_recently_with_present_questions(self):
+        time                     = timezone.now() - datetime.timedelta(hours=23)
+        
+        present_question          = self.question
+        present_question.pub_date = time
+
+        # self.assertIs(present_question.was_published_recently(), False)
+        self.assertTrue(present_question.was_published_recently())
+
+    def test_was_published_recently_with_past_questions(self):
+        time                     = timezone.now() - datetime.timedelta(days=1, minutes=1)
+        
+        past_question          = self.question
+        past_question.pub_date = time
+
+        # self.assertIs(past_question.was_published_recently(), False)
+        self.assertFalse(past_question.was_published_recently())
+
+
+    def test_was_published_recently_with_today_questions(self):
+        time                     = timezone.now()
+        
+        past_question          = self.question
+        past_question.pub_date = time
+
+        # self.assertIs(past_question.was_published_recently(), False)
+        self.assertTrue(past_question.was_published_recently())
